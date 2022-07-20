cc035723fd56488ab8f2e6d5952f657913d7401a
sabrina <sabrinaabrodi20@gmail.com>
2022-07-20 15:38:14 +0200

    importing and adding data function

--- music-player-react/src/App.js
+++ music-player-react/src/App.js
@@ -1,7 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+// Import Styles
+import "./style/style.scss";
+
+// Adding Components
+import Player from "./components/player";
+import Song from "./components/song";
+//import Util
+import data from "./data";
 
 function App() {
-  return <div className="App"></div>;
+  //State
+  const [songs, setSongs] = useState(data());
+  return (
+    <div className="App">
+      <Song />
+      <Player />
+    </div>
+  );
 }
 
 export default App;
